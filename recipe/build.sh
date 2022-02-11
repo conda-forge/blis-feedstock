576a2f1e24a13341afb27ec6cb26889308631b4c
Gaël Donval <gdonval@pm.me>
2022-02-11 15:53:48 +0000

    Revert to using main dir as build dir

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,18 @@
+#!/bin/sh
 CFLAGS=$(echo "${CFLAGS}" | sed "s/-march=[a-zA-Z0-9]*//g")
 CFLAGS=$(echo "${CFLAGS}" | sed "s/-mtune=[a-zA-Z0-9]*//g")
 
 # Avoid sorting LDFLAGS
 sed -i.bak 's/LDFLAGS := $(sort $(LDFLAGS))//g' common.mk
 
-mkdir build_workdir && cd build_workdir
-../configure --prefix=$PREFIX --disable-static --enable-shared --enable-cblas --enable-threading=$threading $CPU_FAMILY
-make -j${CPU_COUNT}
-make install
-make check -j${CPU_COUNT}
+if [[ "$CC" == *"-gnu-"* ]]
+then
+  CC_VENDOR=gcc
+else
+  CC_VENDOR=clang
+fi
+
+./configure --prefix=$PREFIX --disable-static --enable-shared --enable-cblas --enable-threading=$threading $CPU_FAMILY
+make CC_VENDOR=$CC_VENDOR -j${CPU_COUNT}
+make CC_VENDOR=$CC_VENDOR install
+make CC_VENDOR=$CC_VENDOR check -j${CPU_COUNT}
