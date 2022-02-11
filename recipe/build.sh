1557fc689f8ce67378239910980a81f74e1ab09e
Gaël Donval <gdonval@pm.me>
2022-02-11 13:42:18 +0000

    Simplify build and add openmp support

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,34 +4,8 @@ CFLAGS=$(echo "${CFLAGS}" | sed "s/-mtune=[a-zA-Z0-9]*//g")
 # Avoid sorting LDFLAGS
 sed -i.bak 's/LDFLAGS := $(sort $(LDFLAGS))//g' common.mk
 
-case $target_platform in
-    osx-*)
-        export CC=$BUILD_PREFIX/bin/clang
-        ./configure --prefix=$PREFIX --enable-cblas --enable-threading=pthreads intel64
-        make CC_VENDOR=clang -j${CPU_COUNT}
-        make install
-        make check -j${CPU_COUNT}
-        ;;
-    linux-*)
-        ln -s `which $CC` $BUILD_PREFIX/bin/gcc
-        export CC=$BUILD_PREFIX/bin/gcc
-        ./configure --prefix=$PREFIX --enable-cblas --enable-threading=pthreads x86_64
-        make CC_VENDOR=gcc -j${CPU_COUNT}
-        make install
-        make check -j${CPU_COUNT}
-        ;;
-    win-*)
-        export LIBPTHREAD=
-        ./configure --disable-shared --enable-static --prefix=$PREFIX --enable-cblas --enable-threading=pthreads --enable-arg-max-hack x86_64
-        make -j${CPU_COUNT}
-        make install
-        make check -j${CPU_COUNT}
- 
-        ./configure --enable-shared --disable-static --prefix=$PREFIX --enable-cblas --enable-threading=pthreads --enable-arg-max-hack x86_64
-        make -j${CPU_COUNT}
-        make install
-        mv $PREFIX/lib/libblis.lib $PREFIX/lib/blis.lib
-        mv $PREFIX/lib/libblis.a $PREFIX/lib/libblis.lib
-        mv $PREFIX/lib/libblis.*.dll $PREFIX/bin/
-        ;;
-esac
+mkdir build && cd build
+../configure --prefix=$PREFIX --disable-static --enable-shared --enable-cblas --enable-threading=$threading $CPU_FAMILY
+make -j${CPU_COUNT}
+make install
+make check -j${CPU_COUNT}
