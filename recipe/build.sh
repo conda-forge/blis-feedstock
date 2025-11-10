c4608c7bf96799d0e6b51cf94c1c4024361d8515
Michał Górny <mgorny@quansight.com>
2025-11-10 18:23:48 +0100

    Enable verbose builds for debugging
    Signed-off-by: Michał Górny <mgorny@quansight.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -61,7 +61,7 @@ esac
 
 
 # General case
-./configure --prefix=$PREFIX --enable-cblas --enable-threading="$MODEL" $EXTRA $arch
+./configure --prefix=$PREFIX --enable-verbose-make --enable-cblas --enable-threading="$MODEL" $EXTRA $arch
 make -j${CPU_COUNT}
 make install
 make check -j${CPU_COUNT}
@@ -69,7 +69,7 @@ make check -j${CPU_COUNT}
 
 # Windows-specific shenanigans (builds twice; first static above, then shared below)
 case $target_platform in win-*)
-    ./configure --enable-shared --disable-static --prefix=$PREFIX --enable-cblas --enable-threading="$MODEL" --enable-arg-max-hack $arch
+    ./configure --enable-shared --disable-static --prefix=$PREFIX --enable-verbose-make --enable-cblas --enable-threading="$MODEL" --enable-arg-max-hack $arch
     make -j${CPU_COUNT}
     make install 
     find $PREFIX/lib -iname "libblis.*.dll" -exec mv {} $PREFIX/bin/ \;
